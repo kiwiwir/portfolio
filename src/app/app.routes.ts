a766c5ca71f4bc044bc2354f230c76a38da5eeb0
kiwiwir <poleczkag@o2.pl>
2026-01-27 13:02:52 +0100

    Added header with buttons to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Domyślnie przekierowanie do Home
+  { path: '**', redirectTo: 'home' }, // Obsługa nieznanych ścieżek
+];
